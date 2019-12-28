d2ad743b02f0ca19af9d748af4111bde974284b4
Uriel Arriaga <joarriag@cisco.com>
2019-12-27 23:11:27 -0600

    2019-12-27:23:11

--- api.py
+++ api.py
@@ -7,11 +7,11 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def get():
-    return "9239238"
+    return "a30ee592b51f2f382a1e9d2dd0baed8920e8d51a"
 
 @app.route('/', methods=['POST'])
 def post():
     print(request.data)
-    return request.data
+    return "a30ee592b51f2f382a1e9d2dd0baed8920e8d51a"
 
-app.run(port=1050,debug=True)
+app.run(port=1050,debug=True,ssl_context=('cert.pem', 'key.pem'))

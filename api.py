6aad1f3a93eae6a1f50a0aef9c6de2b2bb5330a4
Uriel Arriaga <joarriag@cisco.com>
2019-12-27 18:28:26 -0600

    2019-12-27:17:47

---
+++ api.py
@@ -0,0 +1,17 @@
+import flask
+from flask import request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def get():
+    return "9239238"
+
+@app.route('/', methods=['POST'])
+def post():
+    print(request.data)
+    return request.data
+
+app.run()

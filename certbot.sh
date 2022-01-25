da61216f410ddac2e4602232a4678e7de97c88c9
uri pi <uri@pi.com>
2022-01-25 00:07:00 -0600

    added certs from letsencrypt

---
+++ certbot.sh
@@ -0,0 +1,1 @@
+sudo docker run -it --rm -p 80:80 --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt"  certbot/certbot:arm32v6-nightly certonly                 --standalone    --email uriel.arriaga@momodevops.dev -d depa-dnkrvqmqhr.dynamic-m.com
--- dockerRun.sh
+++
@@ -1,3 +0,0 @@
-docker stop testFlask 
-docker rm testFlask
-docker run --name testFlask -d -p 80:5000 flask-docker

780c00c5ca044a0e8ebc1ef2607aef1e2f2eda8f
uri arriaga <uriel.arriaga@momodevops.dev>
2022-01-25 00:21:23 -0600

    Format certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -1,1 +1,7 @@
-sudo docker run -it --rm -p 80:80 --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt"  certbot/certbot:arm32v6-nightly certonly                 --standalone    --email uriel.arriaga@momodevops.dev -d depa-dnkrvqmqhr.dynamic-m.com
+sudo docker run -it --rm -p 80:80 --name certbot \
+                -v "/etc/letsencrypt:/etc/letsencrypt" \
+                -v "/var/lib/letsencrypt:/var/lib/letsencrypt"  \
+                certbot/certbot:arm32v6-nightly certonly  \
+                --standalone   \
+                --email uriel.arriaga@momodevops.dev \
+                -d depa-dnkrvqmqhr.dynamic-m.com

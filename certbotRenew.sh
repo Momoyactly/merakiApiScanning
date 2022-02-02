0985fb8994c257a8f3614e2c030a042e1e0a9098
uri pi <uri@pi.com>
2022-02-02 13:48:54 -0600

    pi local changes

--- certbotRenew.sh
+++ certbotRenew.sh
@@ -1,7 +1,4 @@
 sudo docker run -it --rm -p 80:80 --name certbot \
                 -v ~/Dev/merakiApiScanning/certs/etc/letsencrypt:/etc/letsencrypt \
                 -v ~/Dev/merakiApiScanning/certs/var/lib/letsencrypt:/var/lib/letsencrypt  \
-                certbot/certbot:arm32v6-nightly certonly  \
-                --standalone  --agree-tos --no-eff-email \
-                --email uriel.arriaga@momodevops.dev \
-                -d uri-dnkrvqmqhr.dynamic-m.com
+                certbot/certbot:arm32v6-nightly  renew --dry-run 
